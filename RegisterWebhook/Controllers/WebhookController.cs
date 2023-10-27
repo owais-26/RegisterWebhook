e33052cc3edbb79d5ce0707f3120903c670e445f
owais-26 <owais.ansaricr7@gmail.com>
2023-10-28 01:57:21 +0500

    Add project files.

---
+++ RegisterWebhook/Controllers/WebhookController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+[ApiController]
+[Route("api/webhook")]
+public class WebhookController : ControllerBase
+{
+    private readonly ILogger<WebhookController> _logger;
+
+    public WebhookController(ILogger<WebhookController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("register-pos")]
+    public IActionResult RegisterPosWebhook(RegisterPosPayload payload)
+    {
+        // Process the incoming payload and generate the response
+        var response = new WebhookUrls
+        {
+            OrdersWebhookURL = "https://integrator.com/ordersWebhookURL",
+            SyncProductsURL = "https://integrator.com/syncProductsURL",
+            SyncTablesURL = "https://integrator.com/syncTablesURL",
+            SyncFloorsURL = "https://integrator.com/syncFloorsURL"
+        };
+
+        // Log the received payload (optional)
+        _logger.LogInformation("Received Register POS webhook request: {Payload}", payload);
+
+        return Ok(response);
+    }
+}
---
+++ RegisterWebhook/Models/ModelDB.cs
@@ -0,0 +1,15 @@
+﻿public class RegisterPosPayload
+{
+    public string AccountId { get; set; }
+    public string LocationId { get; set; }
+    public string ExternalLocationId { get; set; }
+    public string LocationName { get; set; }
+}
+
+public class WebhookUrls
+{
+    public string OrdersWebhookURL { get; set; }
+    public string SyncProductsURL { get; set; }
+    public string SyncTablesURL { get; set; }
+    public string SyncFloorsURL { get; set; }
+}
